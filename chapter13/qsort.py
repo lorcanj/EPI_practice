856d47865913b002a0e4d3b653c0e77b85f3802d
Lorcan <lorcan.james@yahoo.com>
2020-12-05 14:43:49 +0000

    added quicksort

--- chapter13/qsort.py
+++ chapter13/qsort.py
@@ -1,20 +1,33 @@
 def sort(A, lo, hi):
     if lo < hi:
-        pivot_position = sort_in_array(A, 0, len(A) - 1)
+        pivot_position = sort_in_array(A, lo, hi)
         sort(A, lo, pivot_position - 1)
-        sort(A, pivot_position + 1, hi)
-    
-    
+        sort(A, pivot_position + 1, hi)    
 
 
 def sort_in_array(A, lo, hi):
-    # we are choosing the pivot
-    i = left
-    j = right - 1
-    pivot = right
 
+    # we are choosing the pivot
+    i = lo
+    j = hi - 1
+    pivot = A[hi]
+
+    while i < j:
+        while A[i] < pivot and i < hi:
+            i += 1
+        while A[j] >= pivot and j > lo:
+            j -= 1
+        if i < j:
+            A[i], A[j] = A[j], A[i]
     
-        
+    if A[i] > pivot:
+        A[i], A[hi] = A[hi], A[i]
+    return i
 
 p = [1, 4, 2, 9, 8, 11, 30, 0, 7]
-print(sort(p, 0 ,len(p) - 1))
+for x in p:
+    print(x)
+print()
+sort(p, 0 ,len(p) - 1)
+for x in p:
+    print(x)
